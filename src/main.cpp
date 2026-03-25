2b222a4200c94852ef52caf540e5fb507815a96e
kittenchilly <23617175+kittenchilly@users.noreply.github.com>
2026-03-25 14:30:40 -0500

    Glow, trail, and shipfire

--- src/main.cpp
+++ src/main.cpp
@@ -76,6 +76,155 @@ void updateFrames(PlayerObject* player)
     gameManager->loadDeathEffect(gameManager->getPlayerDeathEffect());
 }
 
+const char* getFrameForStreak(int shipFire, float delta)
+{
+    int spritesCount;
+    float timeStep;
+    if (shipFire == 4)
+    {
+        timeStep = 5.0/12;
+    }
+    else if (shipFire == 5)
+    {
+        timeStep = 0.05;
+    }
+    else if (shipFire == 6)
+    {
+        timeStep = 5.0/12;
+    }
+    else
+    {
+        timeStep = 3.0/96;
+    }
+
+    if (shipFire == 2)
+    {
+        spritesCount = 9;
+    }
+    else if (shipFire == 3)
+    {
+        spritesCount = 10;
+    }
+    else
+    {
+        if (shipFire == 4) spritesCount = 6;
+        else if (shipFire == 5) spritesCount = 16;
+        else if (shipFire == 6) spritesCount = 5;
+        else spritesCount = 0;
+    }
+
+    if (spritesCount == 0) return "";
+    int step = (int)floorf(delta / timeStep);
+    int spriteStep = step % spritesCount + 1;
+
+    return CCString::createWithFormat("shipfire%02d_%03d.png", shipFire, spriteStep)->getCString(); // org actually uses fmt but i cba
+}
+
+void normalStreak(PlayerObject* player)
+{
+    auto gameManager = GameManager::sharedState();
+
+    player->m_streakStrokeWidth = 10.0;
+    player->m_alwaysShowStreak = false;
+
+    float streakFade = 0.3;
+    float streakStroke = 10.0;
+    switch (gameManager->getPlayerStreak())
+    {
+        case 2:
+        case 7:
+            streakStroke = 14.0;
+            player->m_disableStreakTint = true;
+            player->m_streakStrokeWidth = 14.0;
+            break;
+        case 3:
+            streakStroke = 8.5;
+            player->m_streakStrokeWidth = 8.5;
+            break;
+        case 4:
+            streakFade = 0.4;
+            streakStroke = 10.0;
+            break;
+        case 5:
+            streakFade = 0.6;
+            streakStroke = 5.0;
+            player->m_streakStrokeWidth = 5.0;
+            player->m_alwaysShowStreak = true;
+            break;
+        case 6:
+            streakFade = 1.0;
+            streakStroke = 3.0;
+            player->m_streakStrokeWidth = 3.0;
+            player->m_alwaysShowStreak = true;
+            break;
+    }
+    
+    player->m_playerStreak = gameManager->getPlayerStreak();
+    player->m_regularTrail->initWithFade(streakFade, 5.0, streakStroke, ccc3(255, 255, 255), CCString::createWithFormat("streak_%02d_001.png", gameManager->getPlayerStreak())->getCString());
+    if (gameManager->getPlayerStreak() == 6)
+    {
+        player->m_regularTrail->enableRepeatMode(0.1);
+    }
+    player->m_regularTrail->m_fMaxSeg = 50.0;
+    player->m_regularTrail->setBlendFunc({GL_SRC_ALPHA, GL_ONE});
+    if (!player->m_disableStreakTint)
+    {
+        player->m_regularTrail->tintWithColor(player->getSecondColor());
+    }
+    if (!player->m_alwaysShowStreak)
+    {
+        player->m_regularTrail->stopStroke();        
+    }
+}
+
+void shipFire(PlayerObject* player)
+{
+    auto gameManager = GameManager::sharedState();
+
+    float streakFade = 0.0;
+    float streakStroke = 0.0;
+    switch (gameManager->getPlayerShipFire())
+    {
+        case 2:
+            streakFade = 0.0636;
+            streakStroke = 22.0;
+            break;
+        case 3:
+            streakFade = 0.1278;
+            streakStroke = 28.6;
+            break;
+        case 4:
+            streakFade = 0.105;
+            streakStroke = 28.6;
+            break;
+        case 5:
+            streakFade = 0.09;
+            streakStroke = 18.7;
+            break;
+        case 6:
+            streakFade = 0.096;
+            streakStroke = 27.0;
+            break;
+    }
+
+    player->m_shipStreakType = static_cast<ShipStreak>(gameManager->getPlayerShipFire());
+
+    if (gameManager->getPlayerShipFire() > 1)
+    {
+        CCTexture2D* texture2d = CCTextureCache::get()->addImage(getFrameForStreak(gameManager->getPlayerShipFire(), 0), false);
+        CCMotionStreak* p = player->m_shipStreak;
+        if (p)
+        {
+            p->setTexture(texture2d);
+            player->m_shipStreak = p;
+        }
+    }
+    else
+    {
+        player->m_shipStreak = nullptr;
+    }
+}
+
 class $modify(PlayLayer) {
 
     void resetLevel() {
@@ -91,25 +240,32 @@ class $modify(PlayLayer) {
         int color1 = gameManager->getPlayerColor();
         int color2 = gameManager->getPlayerColor2();
         int glowColor = gameManager->getPlayerGlowColor();
+        int glow = gameManager->getPlayerGlow();
 
         m_player1->setColor(gameManager->colorForIdx(color1));
         m_player2->setColor(gameManager->colorForIdx(color2));
-
         m_player1->setSecondColor(gameManager->colorForIdx(color2));
         m_player2->setSecondColor(gameManager->colorForIdx(color1));
-
-        if (gameManager->getPlayerGlow())
-        {
+        m_player1->toggleGhostEffect(m_player1->m_ghostType);
+        m_player2->toggleGhostEffect(m_player2->m_ghostType);
+        m_player1->m_hasGlow = glow;
+        m_player2->m_hasGlow = glow;
+        if (m_player1->m_hasGlow)
             m_player1->enableCustomGlowColor(gameManager->colorForIdx(glowColor));
-            m_player2->enableCustomGlowColor(gameManager->colorForIdx(color1));
-        }
         else
-        {
             m_player1->disableCustomGlowColor();
+        if (m_player2->m_hasGlow)
+            m_player2->enableCustomGlowColor(gameManager->colorForIdx(glowColor));
+        else
             m_player2->disableCustomGlowColor();
-        }
+        m_player1->updatePlayerGlow();
+        m_player2->updatePlayerGlow();
         m_player1->updateGlowColor();
         m_player2->updateGlowColor();
+        normalStreak(m_player1); // why can't it be simple
+        normalStreak(m_player2);
+        shipFire(m_player1);
+        shipFire(m_player2);
 
         if (separateDualIconsLoaded)
         {
@@ -122,7 +278,8 @@ class $modify(PlayLayer) {
             cocos2d::CCObject* obj;
             auto colorActions = effectManager->getAllColorActions();
 
-            if (colorActions) {
+            if (colorActions)
+            {
                 cocos2d::CCObject* obj; 
                 for (auto obj : CCArrayExt(colorActions))
                 {
